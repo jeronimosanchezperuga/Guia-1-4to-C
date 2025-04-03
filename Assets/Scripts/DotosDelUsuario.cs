4cd4206159ac4866b89ca06cbec62520195c06fd
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2025-04-03 11:40:46 -0300

    Guia 1 Ej 3 Calculos

--- Assets/Scripts/DotosDelUsuario.cs
+++ Assets/Scripts/DotosDelUsuario.cs
@@ -12,7 +12,8 @@ public class DotosDelUsuario : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Mi nombre es " + nombreUsuario);
+        Debug.Log("Mi nombre es " + nombreUsuario + " tengo " + edadUsuario + ", mido " + alturaUsuario + " metros y es"
+           + esDonante + " que soy donante de órganos." );
     }
 
     // Update is called once per frame
